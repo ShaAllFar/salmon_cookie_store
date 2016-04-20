d2136b19036632bd40fb99b93f2fb80e76bf6c6e
ercain03 <ercain3@gmail.com>
2016-04-20 16:02:18 -0700

    new edits for weds

--- js/app.js
+++ js/app.js
@@ -1,57 +1,93 @@
-var hours = [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8];
+var times = ['6am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
 
 var appendBody = document.getElementById('append-table-row');
+var wholeTable = document.getElementById('whole-table');
+
 var stores = [];
 
-function randomNumber(min,max){
-  return Math.floor(Math.random() * (max - min) + 1) + min;
-};
+function getRandomIntInclusive(min,max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-function storeDataCreator(name, min, max, avgCookiePerCust){
+function storeLocation(name, min, max, avgCooksPerCustomer){
   this.name = name;
   this.min = min;
   this.max = max;
-  this.avgCookiePerCust = avgCookiePerCust;
-  this.resultsArray = [];
+  this.avgCooksPerCustomer = avgCooksPerCustomer;
+  this.arrayA = [];
   this.storeCount = 0;
-  this.cookiesPerHour = function(){
-    for(var i = 0; i < hours.length - 4; i++){
-      var time = 'am';
-      var randomNum = randomNumber(this.min, this.max);
-      var cookiesPurchasedEachHour = Math.floor(this.avgCookiePerCust * randomNum);
-      if(i > 5)
-      {
-        time = 'pm';
-      }
-      this.resultsArray.push(cookiesPurchasedEachHour);
-      this.storeCount += cookiesPurchasedEachHour;
-    }
+  this.avgPurchasePerHr = function(){
+    for(var i = 0; i < times.length - 1; i++) {
+      var randomInt = getRandomIntInclusive(this.min,this.max);
+      var results = Math.floor(randomInt * this.avgCooksPerCustomer);
+      this.arrayA.push(results);
+      this.storeCount += results;
+    };
   };
-  this.cookiesPerHour();
-};
+  this.avgPurchasePerHr();
+}
 
-function makeLocationRow(storeDataCreator){
+function makeLocationRow(storeObj){
   var tr = document.createElement('tr');
   var th = document.createElement('th');
-  th.textContent = storeDataCreator.name;
+  th.textContent = storeObj.name;
   tr.appendChild(th);
-  for(var i = 0; i < storeDataCreator.resultsArray.length; i++){
+  for(var i = 0; i < storeObj.arrayA.length; i++){
     var td = document.createElement('td');
-    td.textContent = storeDataCreator.resultsArray[i];
+    td.textContent = storeObj.arrayA[i];
     tr.appendChild(td);
   }
   var td = document.createElement('td');
-  td.textContent = storeDataCreator.storeCount;
+  td.textContent = storeObj.storeCount;
   tr.appendChild(td);
+  console.log('tr ' + tr);
   appendBody.appendChild(tr);
 }
 
-stores.push(new storeDataCreator('Pike Place', 17, 88, 5.2));
-stores.push(new storeDataCreator('Seatac Airport', 6, 24, 1.2));
-stores.push(new storeDataCreator('Southcenter', 11, 38, 1.9));
-stores.push(new storeDataCreator('Bellevue Square', 20, 48, 3.3));
-stores.push(new storeDataCreator('Alki', 3, 24, 2.6));
+stores.push(new storeLocation('Pike Place',17,88,5.2));
+stores.push(new storeLocation('Seatac Airport',6,24,1.2));
+stores.push(new storeLocation('Southcenter',11,38,1.9));
+stores.push(new storeLocation('Bellevue Square',20,48,3.3));
+stores.push(new storeLocation('Alki',3,24,2.6));
 
-for(var i = 0; i < stores.length; i++){
+for(var i = 0; i < stores.length; i++) {
   makeLocationRow(stores[i]);
 }
+
+var salesForm = document.getElementById('submission_form');
+
+function handleFormInputs(event){
+  event.preventDefault();
+
+  var location = event.target.storLocation.value;
+  var minCusty = event.target.minCustomer.value;
+  var maxCusty = event.target.maxCustomer.value;
+  var avgCusty = event.target.avgCustomer.value;
+  var storeFound = false;
+
+  for(var i = 0; i < stores.length; i++){
+    if(location === stores[i].name){
+      stores[i].min = minCusty;
+      stores[i].max = maxCusty;
+      stores[i].avgCooksPerCustomer = avgCusty;
+      storeFound = true;
+    }
+  }
+  if(storeFound === true){
+    console.log(appendBody);
+    var tableBody = document.getElementById('append-table-row');
+    appendBody.innerHTML = '';
+    for(var i = 0; i < stores.length; i++){
+      console.log('the table loop fired',i);
+      makeLocationRow(stores[i]);
+    }
+  }
+
+  if(!storeFound){
+    var newObj = new storeLocation(location,minCusty,maxCusty,avgCusty);
+    stores.push(newObj);
+    makeLocationRow(newObj);
+  }
+}
+
+salesForm.addEventListener('submit', handleFormInputs);
